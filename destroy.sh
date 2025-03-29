490629861484bacfabef2579df7f0ddfd17cedbf
iagoDominguezCamean <iago.dominguez.camean@udc.es>
2025-03-29 18:18:37 +0100

    Update

---
+++ destroy.sh
@@ -0,0 +1,5 @@
+#! /bin/sh
+
+terraform -chdir="terraform/appgtw" destroy --auto-approve
+terraform -chdir="terraform/cilium" destroy --auto-approve
+terraform -chdir="terraform/kubenet" destroy --auto-approve
