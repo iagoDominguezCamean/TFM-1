58897db4c81ee1e09b69d3ce50671d5d35be3f24
iagoDominguezCamean <iago.dominguez.camean@udc.es>
2025-05-05 23:42:07 +0200

    Update

--- terraform/cilium/monitoring.tf
+++ terraform/cilium/monitoring.tf
@@ -1,13 +1,19 @@
-# resource "azurerm_dashboard_grafana" "grafana" {
-#   name                  = "aks-grafana"
-#   resource_group_name   = var.resource_group_name
-#   location              = var.location
-#   grafana_major_version = 11
-#   api_key_enabled       = true
-#   deterministic_outbound_ip_enabled = true
-#   public_network_access_enabled     = true
+resource "azurerm_monitor_workspace" "prometheus" {
+  resource_group_name = var.resource_group_name
+  location            = var.location
+  name                = "prometheus"
+}
 
-#   identity {
-#     type = "SystemAssigned"
-#   }
-# }
+resource "azurerm_dashboard_grafana" "grafana" {
+  name                              = "aks-grafana"
+  resource_group_name               = var.resource_group_name
+  location                          = var.location
+  grafana_major_version             = 11
+  api_key_enabled                   = true
+  deterministic_outbound_ip_enabled = true
+  public_network_access_enabled     = true
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
