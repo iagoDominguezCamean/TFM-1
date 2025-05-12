ae582301a9dc218fc9de96387cdab1b09306bb03
iagoDominguezCamean <iago.dominguez.camean@udc.es>
2025-05-12 21:00:44 +0200

    Update

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,13 @@
 #! /bin/sh
 
+terraform -chdir="terraform/acr" init
+terraform -chdir="terraform/kubenet" init
+terraform -chdir="terraform/cilium" init
+terraform -chdir="terraform/appgtw" init
+
 echo "Creating Azure Container registry."
 terraform -chdir="terraform/acr" apply --auto-approve
-if [[$? -eq 0]]; then
+if [ $? -eq 0 ]; then
     echo "ACR created successfully!"
 else
     echo "[ERROR] Creating Azure Container registry. Exiting!"
@@ -11,7 +16,7 @@ fi
 
 echo "Deploying kubenet cluster..."
 terraform -chdir="terraform/kubenet" apply --auto-approve
-if [[$? -eq 0]]; then
+if [ $? -eq 0 ]; then
     echo "Deployment successfull!"
 else
     echo "[ERROR] Kubenet cluster deployment failure."
@@ -20,7 +25,7 @@ fi
 echo "Deploying Cilium cluster..."
 terraform -chdir="terraform/cilium" apply --auto-approve
 
-if [[$? -eq 0]]; then
+if [ $? -eq 0 ]; then
     echo "Deployment successfull!"
 else
     echo "[ERROR] Cilium cluster deployment failure."
@@ -29,7 +34,7 @@ fi
 echo "Installing Cilium..."
 terraform -chdir="terraform/cilium" apply --auto-approve -var="install_cilium=true"
 
-if [[$? -eq 0]]; then
+if [ $? -eq 0 ]; then
     echo "Cilium installed successfully!"
 else
     echo "[ERROR] Failure during Cilium installation!"
@@ -38,7 +43,7 @@ fi
 echo "Deploying Application gateway..."
 terraform -chdir="terraform/appgtw" apply --auto-approve
 
-if [[$? -eq 0]]; then
+if [ $? -eq 0 ]; then
     echo "¡Deployment complete!"
 else
     echo "[ERROR] Failure at Application gateway deployment."
