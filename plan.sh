39bc7632092d8cfa47b532937b82da4d19fc6f55
iagoDominguezCamean <iago.dominguez.camean@udc.es>
2025-05-12 19:46:23 +0200

    Delete plan

--- plan.sh
+++
@@ -1,5 +0,0 @@
-terraform -chdir="terraform/acr" plan
-terraform -chdir="terraform/kubenet" plan
-terraform -chdir="terraform/cilium" plan
-terraform -chdir="terraform/cilium" plan -var="install_cilium=true"
-terraform -chdir="terraform/appgtw" plan
