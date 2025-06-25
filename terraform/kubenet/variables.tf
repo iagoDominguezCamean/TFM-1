8ecff6c5cef240aa0a04337e012068f78d2032f7
iagodc <72920871+iagoDominguezCamean@users.noreply.github.com>
2025-06-26 00:54:23 +0200

    Update variables.tf

--- terraform/kubenet/variables.tf
+++ terraform/kubenet/variables.tf
@@ -76,3 +76,10 @@ variable "address_prefixes" {
   type        = list(string)
   default     = ["10.0.0.0/24"]
 }
+
+# ACR
+variable "acr_name" {
+  description = "Name for the azure container registry"
+  type        = string
+  default     = "acridomingc"
+}
