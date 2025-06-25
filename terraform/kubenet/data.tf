3f64206b1c0135f9fc73ba4213f58e67083fb103
iagodc <72920871+iagoDominguezCamean@users.noreply.github.com>
2025-06-26 00:53:55 +0200

    Update data.tf

--- terraform/kubenet/data.tf
+++ terraform/kubenet/data.tf
@@ -1,1 +1,10 @@
+data "azurerm_client_config" "current" {}
 
+data "azurerm_resource_group" "rg" {
+  name = var.resource_group_name
+}
+
+data "azurerm_container_registry" "acr" {
+  resource_group_name = var.resource_group_name
+  name                = var.acr_name
+}
