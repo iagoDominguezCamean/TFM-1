68d9d3227c799626742f5c4610632fce661a10a9
iagoDominguezCamean <iago.dominguez.camean@udc.es>
2025-07-09 13:00:55 +0200

    Update

--- terraform/cilium_cluster_resources/cilium.tf
+++ terraform/cilium_cluster_resources/cilium.tf
@@ -21,4 +21,51 @@ resource "helm_release" "cilium" {
     name  = "hubble.relay.enabled"
     value = var.hubble_relay_enabled
   }
+  set {
+    name  = "hubble.enabled"
+    value = "true"
+  }
+  set {
+    name  = "hubble.metrics.enabled"
+    value = "{dns,drop,tcp,flow,port-distribution,icmp,http}"
+  }
+  set {
+    name  = "hubble.metrics.enableOpenMetrics"
+    value = "true"
+  }
+  set {
+    name  = "prometheus.enabled"
+    value = "true"
+  }
+  set {
+    name  = "prometheus.port"
+    value = "9962"
+  }
+  set {
+    name  = "operator.prometheus.enabled"
+    value = "true"
+  }
+}
+
+resource "random_password" "grafana_password" {
+  length = 14
+}
+
+# Prometheus and Grafana install
+resource "helm_release" "kube-prometheus-stack" {
+  chart            = "kube-prometheus-stack"
+  repository       = "https://prometheus-community.github.io/helm-charts"
+  name             = "kube-prometheus-stack"
+  namespace        = "monitoring"
+  create_namespace = true
+
+  set {
+    name  = "grafana.adminUser"
+    value = "admin"
+  }
+
+  set {
+    name  = "grafana.adminPassword"
+    value = "admin"
+  }
 }
